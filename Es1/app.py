142326e9cd802296dc8051568c16a13940824630
Wallo03 <walid.birdaha03@gmail.com>
2023-04-01 01:30:58 +0200

    up

--- Es1/app.py
+++ Es1/app.py
@@ -10,3 +10,14 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+boy = 13
+patente = False
+
+if boy >= 18 and patente==True:
+    print("Daje vezz ce l'hai gia la patente")
+elif boy >=18 and patente==False:
+    print("Daje oooh, sta patentina quando al prendiamo?")
+else:
+    print("Sus Bro, troppo cinno")
